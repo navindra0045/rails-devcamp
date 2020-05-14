0a119dc500464af0632ee1a4f92e3f68bdb8aedb
navindra persaud <35715817+navindra0045@users.noreply.github.com>
2020-05-14 08:03:09 -0500

    built new and create action for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,28 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'your portfolio item is now live.' }
+        # format.html { redirect_to @portfolio_item, notice: 'your portfolio item is now live.' }
+        # format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+        # format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+
+    # private
+    #   def portfolio_params
+    #     params.require(:portfolio).permit(:title, :subtitle, :body)
+    #   end
+  end
+
 end
